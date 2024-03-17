5ab63312bd5378192841c0949f9901b7c0f4a224
Aaron Spiteri <azzmosphere@gmail.com>
2024-03-17 18:17:32 +1100

    integrated sonar

--- include/ryderrobot.h
+++ include/ryderrobot.h
@@ -7,17 +7,33 @@
 
 #include <Arduino.h>
 #include <U8glib.h>
+#include <HCSR04.h>
+
+// Custom type definitions
+typedef unsigned long ulong_t;
 
 /**
  * PIN layouts,  
  */
+
+// Pins for OLED
 const uint8_t OLED_CLK = 13;  // OLED CLK connected to Arduino pin 13
 const uint8_t OLED_MOSI = 11; // OLED MOSI connected to Arduino pin 11
 const uint8_t OLED_DC = 9;    // OLED DC connected Arduino pin 9
 const uint8_t OLED_CS = 10;   // OLEDCS connected Arduino pin 10 
 
+// Pins for HCSR04
+const uint8_t RR_HCSR04_TRIG = 4;  // trigger pin
+const uint8_t RR_HCSR04_ECHO = 2;  // echo pin
+
 // pixel offset, for each new line.
 const int OLED_PRINT_OFFSET = 15;
+
+
+// baud - a unit of transmission speed equal to the number of times a signal changes state per second. 
+// For signals with only two possible states one baud is equivalent to one bit per second.
+const ulong_t BUAD_RATE = 9600;
+
 
 /*
  * OLED setup routine
--- test/test_hcsr04/test_ultrasonic.cpp
+++ test/test_hcsr04/test_ultrasonic.cpp
@@ -1,2 +1,32 @@
-#include <Ultrasonic.h>
+#include <HCSR04.h>
+#include <ryderrobot.h>
+#include <unity.h>
 
+void setup() 
+{
+    UNITY_BEGIN();
+    Serial.begin(BUAD_RATE);
+    HCSR04.begin(RR_HCSR04_TRIG, RR_HCSR04_ECHO);
+}
+
+void tearDown(void)
+{
+  // clean stuff up here
+}
+
+void getDistance() 
+{
+    double* distances = HCSR04.measureDistanceCm();
+    Serial.print("1: ");
+    Serial.print(distances[0]);
+    Serial.println(" cm");
+  
+    Serial.println("---");
+}
+
+void loop()
+{
+    RUN_TEST(getDistance);
+    UNITY_END(); // stop unit testing
+  
+}
